af7e4c9d9d58d2f75888a61b93603d46cd2d2ba9
brainboxemb <285184499+brainboxemb@users.noreply.github.com>
2026-09-24 21:05:21 +0200

    refactor: remove premature status model

--- framework/src/main/java/io/github/brainboxemb/eventtiming/application/ProblemSeverity.java
+++
@@ -1,7 +0,0 @@
-package io.github.brainboxemb.eventtiming.application;
-
-/** Severity values for observable first-executable application problems. */
-public enum ProblemSeverity {
-    WARNING,
-    ERROR
-}
