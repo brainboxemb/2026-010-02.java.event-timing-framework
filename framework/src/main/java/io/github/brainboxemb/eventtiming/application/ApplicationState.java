c8fd5cda65a653b930ba009ab2c541c6e3042f7f
brainboxemb <285184499+brainboxemb@users.noreply.github.com>
2026-09-24 21:05:13 +0200

    refactor: remove premature status model

--- framework/src/main/java/io/github/brainboxemb/eventtiming/application/ApplicationState.java
+++
@@ -1,9 +0,0 @@
-package io.github.brainboxemb.eventtiming.application;
-
-/** First-executable application states defined by the IF-03 status contract. */
-public enum ApplicationState {
-    STARTING,
-    RUNNING,
-    DEGRADED,
-    STOPPING
-}
